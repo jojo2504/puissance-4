e05553fc97160498d3a6c98ca9dec64da6b858af
jojo2504 <jojo.duchuy@gmail.com>
2025-10-02 23:06:04 +0200

    feat: board creation and push

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
+struct Board {
+    height: i32,
+    width: i32,
+    board: Vec<Vec<Option<char>>>
+}
+
+impl Board {
+    pub fn new(height: i32, width: i32) -> Self {
+        Self {
+            height: height,
+            width: width,
+            board: vec![vec![Some('#'); width as usize]; height as usize]
+        }
+    }
+
+    pub fn push(&mut self, col: i32) {
+        if col < 1 || col > self.width-1 {
+            println!("invalid input");
+            return;
+        }
+        
+        let mut row = self.height-1;
+        while self.board[row as usize][(col-1) as usize].unwrap() != '#' {
+            row -= 1;
+        }
+        self.board[row as usize][(col-1) as usize] = Some('S');
+    }
+
+    pub fn display_board(& self) {
+        //println!("height is {}", self.height);
+        //println!("width is {}", self.width);
+        for i in 0..self.board.len() { // height
+            for j in 0..self.board[0].len() { // width
+                print!("{}", self.board[i][j].unwrap());
+            }
+            println!();
+        }
+    }
+}
+
+struct Game {
+
+}
+
+impl Game {
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut board = Board::new(5, 10);
+
+    board.push(2);
+    board.display_board();
 }
